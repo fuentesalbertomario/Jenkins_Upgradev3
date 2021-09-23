3f915b74b75e5208d8491417fb1019ff34bf22ce
fuentesalbertomario <85051260+fuentesalbertomario@users.noreply.github.com>
2021-09-23 00:49:04 -0400

    Update TestGreeter.java
    Adding another message

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+      @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "Hi everyone! We are becomign devops";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
